014d7bde9549ee698b30a911796b0d4716154d67
Matteo <matteo.sciacca@etu.univ-amu.fr>
2023-05-15 16:56:09 +0200

    ex5

--- src/main/java/com/example/exercice5/Pacman.java
+++ src/main/java/com/example/exercice5/Pacman.java
@@ -34,12 +34,18 @@ public class Pacman extends Personnage {
 
     @Override
     public void deplacerEnBas(double hauteurJeu) {
+        super.deplacerEnBas(hauteurJeu);
         // à compléter
+        bouche.setEndX(bouche.getStartX());
+        bouche.setEndY(bouche.getStartY() - LARGEUR_MOITIE_PERSONNAGE - .5);
     }
 
     @Override
     public void deplacerEnHaut() {
+        super.deplacerEnHaut();
         // à compléter
+        bouche.setEndX(bouche.getStartX());
+        bouche.setEndY(bouche.getStartY() + LARGEUR_MOITIE_PERSONNAGE + .5);
     }
 
 }
